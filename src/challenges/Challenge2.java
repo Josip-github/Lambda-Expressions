08630cfc20c38226a0dcfd0c152fc96196d73817
Josip-github <josipcota1@gmail.com>
2022-02-18 17:24:13 +0100

    Challenge 3: putting lambdaFunction inside of the main so it can be executed

--- src/challenges/Challenge2.java
+++ src/challenges/Challenge2.java
@@ -17,18 +17,23 @@ public class Challenge2 {
 
     }
 
-    Function<String, String> lambdaFunction = s -> {
-        StringBuilder returnVal = new StringBuilder();
-        for(int i = 0; i < s.length(); i++){
-            if(i % 2 == 1){
-                returnVal.append(s.charAt(i));
-            }
-        }
-        return returnVal.toString();
-    };
+
+
+
 
     public static void main(String[] args) {
 
+        Function<String, String> lambdaFunction = s -> {
+            StringBuilder returnVal = new StringBuilder();
+            for(int i = 0; i < s.length(); i++){
+                if(i % 2 == 1){
+                    returnVal.append(s.charAt(i));
+                }
+            }
+            return returnVal.toString();
+        };
+
+        System.out.println(lambdaFunction.apply("1234567890"));
 
     }
 
